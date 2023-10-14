c1bea9048b9df5728f87d70fc6f1d9da2af8dc49
Naufal Alief <naufalalief087@gmail.com>
2023-10-14 20:06:17 +0700

    mobile first swiper

--- js/script.js
+++ js/script.js
@@ -4,3 +4,13 @@ const mobileNavigation = document.getElementById('mobile-nav');
 toggleBtn.addEventListener('click', function(){
   mobileNavigation.classList.toggle('hidden');
 })
+
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
