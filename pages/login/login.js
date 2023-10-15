5eed2b5a1cb5c0f3b07f07fb6d439200642a0f14
alfitosantosa <44111010166@student.mercubuana.ac.id>
2023-10-16 00:51:01 +0700

    fix login

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,19 +1,37 @@
 let btnSend = document.getElementById("btn-login");
 
 btnSend.addEventListener("click", () => {
-  let newUser = {
-    email: document.getElementById("email").value,
-    password: document.getElementById("password").value,
-  };
+  // Get the email and password from the input fields
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
 
-  fetch("https://65274ef6917d673fd76d8edf.mockapi.io/usersFurnivul", {
-    method: "GET", // or 'PUT'
+  // Assuming you want to send the email as a query parameter to the API
+  let apiUrl = `https://65274ef6917d673fd76d8edf.mockapi.io/usersFurnivul`;
+
+  // Send a GET request to the API
+  fetch(apiUrl, {
+    method: "GET",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify(newUser),
-  }).then((res) => {
-    alert("akun anda berhasil dibuat");
-    console.log(res);
-  });
+  })
+    .then((res) => {
+      if (res.status === 200) {
+        return res.json(); // Parse the JSON response
+      } else {
+        alert("User not found");
+      }
+    })
+    .then((data) => {
+      // Check if the password matches the retrieved user's password
+      if (data && data.length > 0 && data[0].password === password) {
+        alert("Login successful");
+        // You can redirect the user or perform other actions on success.
+      } else {
+        alert("Login failed");
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
 });
