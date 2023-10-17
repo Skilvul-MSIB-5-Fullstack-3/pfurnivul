4637c8c4b3db3f9001309f2fd0da73486ecb8f32
alfitosantosa <44111010166@student.mercubuana.ac.id>
2023-10-17 23:08:31 +0700

    listing fix

--- pages/product-listing/product-listing.js
+++ pages/product-listing/product-listing.js
@@ -1,112 +1,30 @@
-url = "https://65274ef6917d673fd76d8edf.mockapi.io/product";
+let productListing = document.getElementById("list_product");
 
-let listProduct = document.getElementById("list_product");
+async function getProductListing() {
+  let responses = await fetch(
+    "https://6524bed5ea560a22a4ea0e3b.mockapi.io/products"
+  );
+  let products = await responses.json();
+  console.log(products);
 
-async function getPokemon() {
-  try {
-    let response = await fetch(url);
-    let pokemons = await response.json();
-
-    pokemons.forEach((item) => {
-      let cardProduct = `
-      <section class="mt-8">
-      <div
-        class="gap-8 flex w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-8"
-      >
-        <a href=""
-          ><img
-            src="${item.product_image}"
-        /></a>
-        <div class="px-5 p-5 mt-8">
-          <div class="flex-end w-8">
-            <a href=""><img src="../assets/BookmarkOutline.svg" alt="" /></a>
-          </div>
-          <a href="#">
-            <h5
-              class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white"
-            >
-             ${item.product_material}
-            </h5>
-          </a>
-          <div class="flex items-center mt-2.5 mb-5">
-            <svg
-              class="w-4 h-4 text-yellow-300 mr-1"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 22 20"
-            >
-              <path
-                d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"
-              />
-            </svg>
-            <svg
-              class="w-4 h-4 text-yellow-300 mr-1"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 22 20"
-            >
-              <path
-                d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"
-              />
-            </svg>
-            <svg
-              class="w-4 h-4 text-yellow-300 mr-1"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 22 20"
-            >
-              <path
-                d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"
-              />
-            </svg>
-            <svg
-              class="w-4 h-4 text-yellow-300 mr-1"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 22 20"
-            >
-              <path
-                d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"
-              />
-            </svg>
-            <svg
-              class="w-4 h-4 text-gray-200 dark:text-gray-600"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 22 20"
-            >
-              <path
-                d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"
-              />
-            </svg>
-            <span
-              class="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3"
-              >5.0</span
-            >
-          </div>
-          <div class="flex items-center justify-between">
-            <span class="text-3xl font-bold text-gray-900 dark:text-white"
-              >${item.price}</span
-            >
-            <a
-              href="#"
-              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-              >Add to cart</a
-            >
-          </div>
-        </div>
-      </div>
-    </section>`;
-      listProduct.innerHTML += cardProduct;
+  products.map((product) => {
+    let price = product.price;
+    let formattedPrice = price.toLocaleString("id-ID", {
+      style: "currency",
+      currency: "IDR",
     });
-  } catch (error) {
-    console.error("Terjadi kesalahan:", error);
-  }
+    let productCard = `
+
+
+${product.product_name}
+
+${product.product_review}
+
+${formattedPrice}
+
+`;
+    productListing.innerHTML += productCard;
+  });
 }
 
-getPokemon();
+getProductListing();
