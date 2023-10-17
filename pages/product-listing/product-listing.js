53786bf99f0a9189bfab38fb68eadc2445b6855b
alfitosantosa <44111010166@student.mercubuana.ac.id>
2023-10-17 22:41:01 +0700

    fix listing products

---
+++ pages/product-listing/product-listing.js
@@ -0,0 +1,5 @@
+const image = document.getElementById("product-image");
+const title = document.getElementById("product-title");
+const price = document.getElementById("product-price");
+const description = document.getElementById("product-description");
+const addToCart = document.getElementById("add-to-cart");
