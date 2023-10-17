a27ca7ac7a6fb229260de2ce631089fa23fcf2f2
alfitosantosa <44111010166@student.mercubuana.ac.id>
2023-10-17 23:45:54 +0700

    listing fix

--- pages/product-listing/product-listing.js
+++ pages/product-listing/product-listing.js
@@ -16,15 +16,15 @@ async function getProductListing() {
     let productCard = `
     
     <div
-    class="gap-8 flex w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-8 mt-4"
+    class="block gap-8 flex w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-2 mt-4"
   >
-    <a href=""
+    <a class="" style="width:400px; height:400px; allign-items:center;" href=""
       ><img
         src="${product.product_image}"
         alt="Whats-App-Image-2023-10-16-at-14-04-23"
         border="0"
     /></a>
-    <div class="px-5 p-5 mt-8">
+    <div class="px-5 p-5  gap-4">
       <div class="flex-end w-8">
         <a href=""><img src="../assets/BookmarkOutline.svg" alt="" /></a>
       </div>
@@ -96,13 +96,13 @@ async function getProductListing() {
           >${product.product_review}</span
         >
       </div>
-      <div class="flex items-center justify-between">
+      <div class="items-center gap-4 mt-4 mb-4">
         <span class="text-3xl font-bold text-gray-900 dark:text-white"
           >${formattedPrice}</span
         >
         <a
-          href="#"
-          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          href=""
+          class="top-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >Add to cart</a
         >
       </div>
