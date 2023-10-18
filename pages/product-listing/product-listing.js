d12c2f43c912c739fc61c41a9f9d1c6250811f23
alfitosantosa <44111010166@student.mercubuana.ac.id>
2023-10-18 12:01:48 +0700

    fixing listing

--- pages/product-listing/product-listing.js
+++ pages/product-listing/product-listing.js
@@ -16,13 +16,13 @@ async function getProductListing() {
     let productCard = `
     
 
-    <div class=" w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mt-4">
-    <a href="#">
+    <div class="flex w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mt-4 my-auto">
+    <a href="">
         <img class="p-8 rounded-t-lg" src="${product.product_image}" alt="product image" />
     </a>
-    <div class="px-5 pb-5">
-        <a href="#">
-            <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">${product.product_name}</h5>
+    <div class="px-8 pb-8 pt-8 mt-8">
+        <a href="">
+            <h5 class="text-xl font-semibold  text-gray-900 dark:text-white">${product.product_name}</h5>
         </a>
         <div class="flex items-center mt-2.5 mb-5">
             <svg class="w-4 h-4 text-yellow-300 mr-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
@@ -44,7 +44,7 @@ async function getProductListing() {
         </div>
         <div class="flex items-center justify-between">
             <span class="text-3xl font-bold text-gray-900 dark:text-white">${formattedPrice}</span>
-            <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
+            <a href="" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
         </div>
     </div>
 </div>
